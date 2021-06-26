1b9a1f687f61583f4fdb87278a8bd0de4fd332f1
Brayan Alonzo <brayan.quialo@gmail.com>
2021-06-25 18:10:31 -0600

    Se agrego la clase Tablero

---
+++ src/backend/Tablero.java
@@ -0,0 +1,10 @@
+package backend;
+
+public class Tablero {
+    private Casilla[][] tablero;    
+
+    public Tablero(int filas, int columnas){
+        tablero = new Casilla[filas][columnas];        
+        
+    }
+}
